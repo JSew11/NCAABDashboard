3fe74be902e95f59948d2db2cc92a2bf0275be22
JSew11 <jseward@zagmail.gonzaga.edu>
2021-11-30 16:57:02 -0800

    Finished NewsStoryCardView

--- app/src/main/java/com/ncaabdashboard/MainActivity.java
+++ app/src/main/java/com/ncaabdashboard/MainActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
  * @author - Joshua Seward
  * @version - 1.0.0
  * Pennant Image - Icons made by Freepik (https://www.freepik.com) from https://www.flaticon.com/
+ * Placeholder Image - https://www.royalcontainers.com/100-years-of-knowledge-connections/placeholder/
  */
 public class MainActivity extends AppCompatActivity {
 
