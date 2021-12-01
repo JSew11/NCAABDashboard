827425f5f9a5cf04bee49ad72d717862288f9fc8
Alan Poblette <39111070+zeintry@users.noreply.github.com>
2021-11-30 20:12:51 -0800

    Created class to make http requests
    This class is meant to make http requests to newsapi.org which we will use since google news api has been deprecated.

--- app/src/main/java/com/ncaabdashboard/NewsAPIFactory.java
+++ app/src/main/java/com/ncaabdashboard/NewsAPIFactory.java
@@ -1,2 +1,93 @@
-package com.ncaabdashboard;public class NewsAPIFactory {
+package com.ncaabdashboard;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class NewsAPIFactory {
+    static final String TAG_SEARCH_NEWS = "SEARCH_NEWS";
+
+    public static StringRequest getLatestNewsByUniversity(Context context, String university) {
+        // TODO: add capability to load this info from api.properties file
+        String api_key = "01d0758178cb4734bdd33854b2eea5ca";
+
+        String url = "https://newsapi.org/v2/top-headlines?api-key=" + api_key +
+                "&country=us" +
+                "&q=" + university + " college basketball" +
+                "&category=sports";
+        try {
+            // setup connection
+            RequestQueue queue = Volley.newRequestQueue(context);
+
+            StringRequest stringRequest = searchNameStringRequest(university, context);
+            stringRequest.setTag(TAG_SEARCH_NEWS);
+
+            queue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            return null;
+        }
+    }
+
+    /**
+     * Creates a StringRequest with the response from an api request
+     * @param nameSearch
+     * @return
+     */
+    private static StringRequest searchNameStringRequest(String nameSearch, Context context) {
+        // TODO: add capability to load this info from api.properties file
+        String api_key = "01d0758178cb4734bdd33854b2eea5ca";
+
+        String url = "https://newsapi.org/v2/top-headlines?api-key=" + api_key +
+                "&country=us" +
+                "&q=" + nameSearch + " college basketball" +
+                "&category=sports";
+
+        // 1st param => type of method (GET/PUT/POST/PATCH/etc)
+        // 2nd param => complete url of the API
+        // 3rd param => Response.Listener -> Success procedure
+        // 4th param => Response.ErrorListener -> Error procedure
+        return new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    // 3rd param - method onResponse lays the code procedure of success return
+                    // SUCCESS
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("DEBUG",  "GOT RESPONSE FROM SITE");
+                        
+                        // try/catch block for returned JSON data
+                        // see API's documentation for returned format
+                        try {
+                            JSONObject result = new JSONObject(response).getJSONObject("list");
+                            int maxItems = result.getInt("end");
+                            JSONArray resultList = result.getJSONArray("item");
+
+                            // catch for the JSON parsing error
+                        } catch (JSONException e) {
+                            Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    } // public void onResponse(String response)
+                }, // Response.Listener<String>()
+                new Response.ErrorListener() {
+                    // 4th param - method onErrorResponse lays the code procedure of error return
+                    // ERROR
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // display a simple message on the screen
+                        Toast.makeText(context, "Food source is not responding (USDA API)", Toast.LENGTH_LONG).show();
+                    }
+                });
+    }
 }
